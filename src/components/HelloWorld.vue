3b13b7df8a9808bd69687972ac429dc9cc30af58
LinJieLinLin <993353454@qq.com>
2018-11-27 10:38:47 +0800

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -57,7 +57,7 @@ export default {
     name: 'HelloWorld',
     data() {
         return {
-            msg: 'Welcome to Your Vue.js App'
+            msg: 'sandbox app'
         };
     },
     methods: {
